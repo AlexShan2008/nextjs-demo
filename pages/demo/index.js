78c9ae6e256c921ae36fda0a6933a6273dabf499
Alex <shantong@reyun.com>
2020-08-20 08:52:22 +0800

    feat: add swr and api endpoints

--- pages/demo/index.js
+++ pages/demo/index.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import useSwr from 'swr';
+// import Link from 'next/link'
 import { Button } from 'antd';
 import styles from '../../styles/Demo.module.scss';
 import { withTranslation, Link } from '../../i18n';
+import { fetcher } from '../../utils';
 
 function Demo({ t }) {
   const [count, setCount] = useState(0);
+  const { data, error } = useSwr('/api/users', fetcher);
+
+  if (error) return <div>Failed to load users</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <div className={styles.root}>
@@ -29,6 +36,19 @@ function Demo({ t }) {
       </main>
 
       <img className={styles.img} src="/static/cartoon.jpeg" alt="my image" />
+
+      <main>
+        <h3>用户列表</h3>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>
+              <Link href="/user/[id]" as={`/user/${user.id}`}>
+                <a>{`User ${user.id}`}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
